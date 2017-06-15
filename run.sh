3c7cad2c546e3289145c20f3058c3047ae40595f
longdt <long.dinhtrong2@vndirect.com.vn>
2017-06-15 16:16:43 +0700

    update run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 JAVA=java
 JAVA_OPS=-Xmx512m
-LIB=mailer-1.0-SNAPSHOT.jar:lib/*
+LIB=mailer-1.0.0-SNAPSHOT.jar:lib/*
 $JAVA $JAVA_OPS -cp $LIB:conf/:templates/._class/ vn.com.vndirect.mail.MailerService &
 echo $! > PID
 
