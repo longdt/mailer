e70fd343a7d5ec9c5de83475160633ee67dc8f7f
longdt <long.dinhtrong2@vndirect.com.vn>
2017-06-19 14:45:41 +0700

    add for_user_created_by_broker template

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 JAVA=java
-JAVA_OPS=-Xmx2g -XX:+UseConcMarkSweepGC
+JAVA_OPS="-Xmx2g -XX:+UseConcMarkSweepGC"
 LIB=mailer-1.0.0-SNAPSHOT.jar:lib/*
 $JAVA $JAVA_OPS -cp $LIB:conf/:templates/._class/ vn.com.vndirect.mail.MailerService &
 echo $! > PID
